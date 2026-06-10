31ed23a78b69b805dceeff65c4baf0477fdb6c82
Steve Benz <s8878992@hotmail.com>
2026-06-09 22:37:19 -0700

    Made a tray icon phone status indicator.

--- TrayIconController.cpp
+++ TrayIconController.cpp
@@ -5,87 +5,40 @@
 #include <QIcon>
 #include <QMenu>
 #include <QAction>
+#include <QDir>
 
 TrayIconController::TrayIconController(SmsBackend &backend, QObject *parent)
     : QObject(parent), m_backEnd(backend)
 {
-    QIcon icon = QIcon::fromTheme("anydesk");
-    auto s = icon.name();
-    m_tray.setIcon(icon);
-
-    m_tray.setToolTip("zomg the tooltip works");
-
-    // m_tray.setIcon(QIcon(":/icons/app-base.svg"));
-    m_tray.show();
-
     QObject::connect(&backend, &SmsBackend::deviceStatusChanged,
                      this, &TrayIconController::refreshIcon);
     QObject::connect(&backend, &SmsBackend::unreadMessageCountChanged,
                      this, &TrayIconController::refreshIcon);
+
+    refreshIcon();
+    m_tray.show();
 }
 
 void TrayIconController::refreshIcon()
 {
-    // LAND MINE!  This depends on having the breeze icon library installed,
-    //   which it won't be for most users or even beta-testers.
-    //   TODO: Copy the icon into a resource to remove this dependency.
-
-    // QPixmap pix = QIcon(":/icons/app-base.svg").pixmap(32, 32);
-    QPixmap pix = QIcon::fromTheme("anydesk").pixmap(32, 32);
-
-    QPainter p(&pix);
-    p.setRenderHint(QPainter::Antialiasing);
-
-    QColor color;
+    QString path = ":/icons/tray_";
     switch (m_backEnd.rawDeviceStatus())
     {
     case SmsBackend::Status::Ok:
-        color = Qt::green;
+        path += "connected";
         break;
     case SmsBackend::Status::DeviceUnreachable:
-        color = Qt::yellow;
+        path += "disconnected";
         break;
     default:
-        color = Qt::red;
+        path += "error";
         break;
     }
 
-    drawBadge(p, color, m_backEnd.unreadMessageCount());
-
-    p.end();
-
-    QString path = QString("/tmp/smsapp-icon-%1.png")
-                        .arg(QRandomGenerator::global()->generate());
-
-    pix.save(path, "PNG");
-
-    m_tray.setIcon(QIcon(pix));
-}
-
-void TrayIconController::drawBadge(QPainter &p, const QColor &color, int count)
-{
-    const int size = 32;
-    const int radius = 8;
-
-    QRect circle(size - radius*2 - 2, 2, radius*2, radius*2);
-
-    p.setBrush(color);
-    p.setPen(Qt::NoPen);
-    p.drawEllipse(circle);
-
-    if (count > 0) {
-        p.setPen(Qt::white);
-        QFont f = p.font();
-        f.setBold(true);
-        f.setPointSize(9);
-        p.setFont(f);
-
-        QString text;
-        if (count <= 9)
-            text = QString::number(count);
-        else
-            text = "+";
-
-        p.drawText(circle, Qt::AlignCenter, text);
-    }
+    path += "_";
+    path += m_backEnd.unreadMessageCount() > 0 ? "msgs" : "nomsgs";
+    path += ".svg";
+    QIcon icon(path);
+    m_tray.setToolTip("zomg the tooltip works");
+    m_tray.setIcon(icon);
 }
