b49efe3431ebfa8fa76e7c8ba28c4afc713914fe
Steve Benz <s8878992@hotmail.com>
2026-07-21 15:49:39 -0700

    Made the fake device respond to hasPlugins the same way the real one does.

--- fake-kdeconnect/fakekdeconnectdevice.cpp
+++ fake-kdeconnect/fakekdeconnectdevice.cpp
@@ -15,6 +15,9 @@ QString FakeKdeConnectDevice::name() const {
 }
 
 bool FakeKdeConnectDevice::hasPlugin(const QString &plugin) const {
-    qInfo() << "device::hasPlugin " << plugin << " called on " << m_info->name << "-- returning true";
-    return true;
+    qInfo() << "device::hasPlugin " << plugin << " called on " << m_info->name << "-- returning" << m_info->reachable;
+    // If the device is unreachable, the real daemon will return false for any 'hasPlugin' query.
+    // We return true for all reachable devices just because we just don't have a need to simulate
+    // non-text-enabled devices.
+    return m_info->reachable;
 }
